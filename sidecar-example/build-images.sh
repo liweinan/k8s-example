d390e2edb41fbd57115603a976bcbc6aa66ca8c4
阿男 <l.weinan@gmail.com>
2025-10-23 23:22:55 +0800

    add proxy

--- sidecar-example/build-images.sh
+++ sidecar-example/build-images.sh
@@ -19,8 +19,8 @@ echo ""
 # 构建主应用镜像
 echo "1. 构建主应用镜像..."
 # 如果需要代理，取消注释下面这行并设置正确的代理地址
-# docker build --build-arg HTTP_PROXY=http://your-proxy:port --build-arg HTTPS_PROXY=http://your-proxy:port -f Dockerfile.main-app -t sidecar-main-app:latest .
-docker build -f Dockerfile.main-app -t sidecar-main-app:latest .
+docker build --build-arg HTTP_PROXY=http://squid.corp.redhat.com:3128 --build-arg HTTPS_PROXY=http://squid.corp.redhat.com:3128 -f Dockerfile.main-app -t sidecar-main-app:latest .
+# docker build -f Dockerfile.main-app -t sidecar-main-app:latest .
 
 if [ $? -ne 0 ]; then
     echo "错误: 主应用镜像构建失败"
@@ -31,8 +31,8 @@ fi
 # 构建 Sidecar 镜像
 echo "2. 构建 Sidecar 镜像..."
 # 如果需要代理，取消注释下面这行并设置正确的代理地址
-# docker build --build-arg HTTP_PROXY=http://your-proxy:port --build-arg HTTPS_PROXY=http://your-proxy:port -f Dockerfile.sidecar -t sidecar-sidecar:latest .
-docker build -f Dockerfile.sidecar -t sidecar-sidecar:latest .
+docker build --build-arg HTTP_PROXY=http://squid.corp.redhat.com:3128 --build-arg HTTPS_PROXY=http://squid.corp.redhat.com:3128 -f Dockerfile.sidecar -t sidecar-sidecar:latest .
+# docker build -f Dockerfile.sidecar -t sidecar-sidecar:latest .
 
 if [ $? -ne 0 ]; then
     echo "错误: Sidecar 镜像构建失败"
